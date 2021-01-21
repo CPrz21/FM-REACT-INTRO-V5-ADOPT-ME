b5eae42ad89aa4c4bc555a858310c20e82a385ba
CPrz21 <faselona@gmail.com>
2021-01-21 11:31:46 -0600

    Template literals and hooks

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "@reach/router";
 import { css } from "@emotion/react";
 
 const NavBar = () => {
+  const [padding, setPadding] = useState(15);
   return (
     <header
+      onClick={ () => setPadding(padding + 15)}
       css={css`
+        padding: ${padding}px;
         background-color: #333;
       `}
     >
       <Link to="/">Adopt me!</Link>
-      <span role="img" aria-label="logo">🐶</span>
+      <span 
+        css={css`
+          font-size:60px;
+        `}
+        role="img" 
+        aria-label="logo"
+      >
+      🐶
+      </span>
     </header>
   )
 }
 
-export default NavBar
+export default NavBar;
